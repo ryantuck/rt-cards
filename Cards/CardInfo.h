0ef2bafe019ca3553cd99aaf44eef53c8cd4bedb
ryantuck <ryanstephentuck@gmail.com>
2014-06-27 15:05:00 -0400

    Add first round of card attributes to data model

--- Cards/CardInfo.h
+++ Cards/CardInfo.h
@@ -12,9 +12,25 @@
 
 @interface CardInfo : NSManagedObject
 
-@property (nonatomic, retain) NSString * title;
-@property (nonatomic, retain) NSString * identifier;
-@property (nonatomic, retain) NSDate * createdDate;
+@property (nonatomic, retain) NSString* title;
+@property (nonatomic, retain) NSString* identifier;
+
+@property (nonatomic, retain) NSDate* createdDate;
+@property (nonatomic, retain) NSDate* lastEditedDate;
+@property (nonatomic, retain) NSDate* reminderDate;
+@property (nonatomic, retain) NSDate* dueDate;
+@property (nonatomic, retain) NSDate* completedDate;
+
 @property (nonatomic, retain) NSString* type;
+
+@property (nonatomic, retain) NSString* action;
+@property (nonatomic, retain) NSString* notes;
+@property (nonatomic, retain) NSMutableArray* tags;
+
+
+// needed for project planning
+@property (nonatomic, retain) NSString* project;
+@property (nonatomic, retain) NSString* waitingOn;
+@property (nonatomic, retain) NSString* neededFor;
 
 @end
